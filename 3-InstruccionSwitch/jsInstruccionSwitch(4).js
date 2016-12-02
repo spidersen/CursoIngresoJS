a05cca21acf85ec655586479c3166d7558c912d5
spidersen <cristian.petersen@gmail.com>
2016-12-02 09:49:42 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,29 @@
 function Mostrar()
 {
-//tomo la edad  
+ 
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
+switch(mesDelAño)
+{
+	case("Febrero"):
+	alert("Tiene 28 días");
+	break;
+		case("Enero"):
+		case("Marzo"):
+		case("Mayo"):
+		case("Julio"):
+		case("Agosto"):
+		case("Octubre"):
+		case("Diciembre"):
+		alert("Tiene 31 días");
+		break;
+			default:
+			alert("Tiene 30 días");
+			break;
+
+}
+	
 	
 	
 

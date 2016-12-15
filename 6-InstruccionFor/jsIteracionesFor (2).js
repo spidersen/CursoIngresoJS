b83b69a7eadb8129aa57252d468ebb767654234e
spidersen <cristian.petersen@gmail.com>
2016-12-15 11:25:45 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,32 @@
 function Mostrar()
 {
 
+for (cont=10 ;cont>0 ;cont--) 
+	{
+	console.log(cont);
+	
+	}
+for (cont=10 ;cont>0 ;cont--) 
+	{
+	console.log(cont);
+	
+	}for (cont=10 ;cont>0 ;cont--) 
+	{
+	console.log(cont);
+	
+	}for (cont=10 ;cont>0 ;cont--) 
+	{
+	console.log(cont);
+	
+	}for (cont=10 ;cont>0 ;cont--) 
+	{
+	console.log(cont);
+	
+	}for (cont=10 ;cont>0 ;cont--) 
+	{
+	console.log(cont);
+	
+	}
+
 
 }
--- 6-InstruccionFor/jsIteracionesFor (3).js
+++ 6-InstruccionFor/jsIteracionesFor (3).js
@@ -1,7 +1,12 @@
 function Mostrar()
 {
 
-var repetciones = prompt("ingrese el número de repeticiones");
+var repeticiones = prompt("ingrese el número de repeticiones");
+repeticiones=parseInt(repeticiones);
+for(cont=0;cont<repeticiones;cont++)
+{
+	console.log("Hola UTN FRA");
+}
 
 
 

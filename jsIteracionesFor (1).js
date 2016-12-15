67eaca4f0f40f7117255d1aaa73de36b9fa014de
spidersen <cristian.petersen@gmail.com>
2016-12-15 11:25:19 -0300

    Add files via upload

---
+++ jsIteracionesFor (1).js
@@ -0,0 +1,15 @@
+function Mostrar()
+{
+
+
+	for (cont=0 ;cont<10 ;cont++) 
+	{
+	console.log(cont);
+	}
+
+
+
+
+}
+
+
---
+++ jsIteracionesFor (3).js
@@ -0,0 +1,13 @@
+function Mostrar()
+{
+
+var repeticiones = prompt("ingrese el número de repeticiones");
+repeticiones=parseInt(repeticiones);
+for(cont=0;cont<repeticiones;cont++)
+{
+	console.log("Hola UTN FRA");
+}
+
+
+
+}//FIN DE LA FUNCIÓN
